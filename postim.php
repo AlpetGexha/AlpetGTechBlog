3ca7c2e92b90221d8504fb1d081444c2987a2e5d
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-13 22:58:08 +0200

    add post info & fix single res

--- postim.php
+++ postim.php
@@ -5,19 +5,30 @@ $sql = "SELECT * from post";
 $result = mysqli_query($db, $sql);
 $row = $result->fetch_assoc();
 ?>
-<?php get_header("AlpetG Tech Blog - " . $row['titulli'] . ""); ?>
+<?php get_header("" . $row['titulli'] . " - AlpetG Tech Blog "); ?>
+
 
 <div class="container mt-5">
+    <!-- <h3 class="tittle"></h3> -->
     <div class="row">
-        <div class="col-lg-8 text-left">
+        <div class="col-lg-8 text-left mt-4">
             <div class="single_post">
-                <div class="row">
-                    <img src="assets/img/<?php echo $row['photo']; ?>" class="img-fluid" alt="image not available" style="width:900px;height:350px">
+                <div class="single_post_info">
+                    <img src="assets/img/<?php echo $row['photo']; ?>" class="img-fluid" alt="image not available" style="width:900px;height:380px">
 
-                    <h2><?php echo $row['titulli']; ?></h2>
+                    <div class="single_post_info_show">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="far fa-calendar-alt"></i><?php echo  date('j F, Y ', strtotime($row['date'])); ?></a>
+                            </li>
 
-                    <p><?php echo $row['body']; ?></p>
+                        </ul>
+                    </div>
                 </div>
+                <h1><?php echo $row['titulli']; ?></h1>
+
+                <p><?php echo $row['body']; ?></p>
             </div>
         </div>
 
@@ -25,5 +36,6 @@ $row = $result->fetch_assoc();
 
     </div>
 </div>
+
 
 <?php get_footer(); ?>
