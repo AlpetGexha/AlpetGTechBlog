1f35c46bd49ec464a3847fa8ea0be8fb8457c885
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-13 16:33:03 +0200

    add kategorit

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 function get_header($title_bar)
 {
     include "assets/php/head.php";
-    echo "<title>$title_bar</title></head>";
+    echo " <br> <title>$title_bar</title></head>";
     include "assets/php/header.php";
 }
 
@@ -19,9 +19,9 @@ function get_post($table)
     require("database/config.php");
     $sql = "SELECT * FROM $table ORDER BY id DESC ";
     if ($result = mysqli_query($db, $sql)) {
-        //count number of rows in query result
+        //Nese nuk ka postime 
         $rowcount = mysqli_num_rows($result);
-        //if no rows returned show no news alert
+
         if ($rowcount == 0) {
             # code...
             echo 'Nuk ka Poste "width:350px;height:250px'; 
@@ -33,19 +33,52 @@ function get_post($table)
                 <div class="col-md-6">
                     <div class="b-grid-top">
                         <div class="card" style="width: 18rem;">
-                            <a href="single.php?id=' . $postitem['id'] . '">
+                            <a href="postim.php?id=' . $postitem['id'] . '">
                                 <img src="assets/img/' . $postitem['photo'] . '" class="card-img-top" alt="Foto">
                             </a>
                             <div class="card-body">
-                            <i class="far fa-clock"></i>'. date('j F, Y ', strtotime($postitem['date']))  .'</a>
-                                <h5 class="card-title"><a href="single.php?id=' . $postitem['id'] . '">' . $postitem['titulli'] . '</a></h5>
-                                    <a class="btn btn-primary" href="single.php?id=' . $postitem['id'] . '">Meso m&euml; Shum&euml;</a>
+                            <i class="far fa-clock"></i>'. date('j F, Y ', strtotime($postitem['date']))  .'
+                                <h5 class="card-title"><a href="postim.php?id=' . $postitem['id'] . '">' . $postitem['titulli'] . '</a></h5>
+                                    <a class="btn btn-success" href="postim.php?id=' . $postitem['id'] . '">Meso m&euml; Shum&euml;</a>
                             </div>
                         </div>
                     </div>
                 </div>
             '
             ;
+        }
+    }
+
+    mysqli_close($db);
+}
+
+
+
+function get_kadegoirt($table,$table1)
+{
+    require("database/config.php");
+    $sql = "SELECT * FROM $table";
+    if ($result = mysqli_query($db, $sql)) {
+       //Nese nuk ka kategori  
+        $rowcount = mysqli_num_rows($result);
+        if ($rowcount == 0) {
+            echo 'Nuk ka kategori';
+        }
+
+        foreach ($result as $categoriescount => $kategorit) {
+            // sa herë secila kategori shfaqet në poste
+            $categoryid = $kategorit['id'];
+            $sql = "SELECT * FROM $table1 WHERE category='$categoryid'";
+            if ($result = mysqli_query($db, $sql)) {
+                //numero kategorit
+                $rowcountkategorit = mysqli_num_rows($result);
+                $getcatcount = $rowcountkategorit;
+            }
+            //shfaq
+            echo '<li class="list-group-item d-flex justify-content-between align-items-center">
+			' . $kategorit['name'] . '
+			<span class="badge badge-success badge-pill">' . $rowcountkategorit . '</span>
+			</li>';
         }
     }
 
