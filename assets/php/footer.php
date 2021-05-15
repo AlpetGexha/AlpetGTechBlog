563683cf34075d03443e9dcc3f1a1d70d04b9939
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-15 17:18:43 +0200

    add footer

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,4 +1,11 @@
+<footer>
+    <h3>AlpetG Tech Blog</h3>
 
+</footer>
+
+<style>
+
+</style>
 
 
 <!-- ------------ Boostrap JS ------------------ -->
@@ -8,3 +15,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <!-- ------------ jQuery------------------ -->
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
