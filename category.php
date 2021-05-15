65e63cdc97103186fd51def25341fa9142dae97c
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-15 15:50:45 +0200

    add breadcrumb

--- category.php
+++ category.php
@@ -9,6 +9,16 @@ $row = $result->fetch_assoc();
 
 <?php get_header("" . $row["emri"] . " - AlpetG Tech Blog"); ?>
 
+<style>
+
+</style>
+
+<ol class="breadcrumb">
+    <li class="breadcrumb-item">
+        <a href="index.php">Home</a>
+    </li>
+    <li class="breadcrumb-item active"><?php echo $row['emri']; ?></li>
+</ol>
 
 
 <?php get_footer(); ?>
