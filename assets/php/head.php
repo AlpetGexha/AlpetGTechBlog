7eb5fdd0f424e49b3525ab473d3bb37100008277
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-18 18:07:31 +0200

    Update head.php

--- assets/php/head.php
+++ assets/php/head.php
@@ -15,6 +15,14 @@
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-
    <!-- ------------ icone ------------------ -->
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+   <script>
+      addEventListener("load", function() {
+         setTimeout(hideURLbar, 0);
+      }, false);
+
+      function hideURLbar() {
+         window.scrollTo(0, 1);
+      }
+   </script>
