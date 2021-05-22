9248109b86663754fc500162ca37401ff039822a
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-22 19:42:51 +0200

    Create admin-navbar.php

---
+++ assets/php/admin-navbar.php
@@ -0,0 +1,87 @@
+<script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+
+
+<nav class="sb-topnav sticky-top navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Admin Pannel</a>
+    <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <!-- Navbar-->
+</nav>
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav sticky-top">
+                    <div class="sb-sidenav-menu-heading">Main</div>
+                    <a class="nav-link" href="../admin/index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                        P&euml;doruesits
+                    </a>
+                    <a class="nav-link" href="../admin/sms_admin.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div>
+                        Mesazhet
+                    </a>
+                    <a class="nav-link" href="../admin/post_admin.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-blog"></i></div>
+                        Postimet
+                    </a>
+
+                    <a class="nav-link" href="../admin/create_pos.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-blog"></i></div>
+                        Krijo Postime
+                    </a>
+
+                    <a class="nav-link" href="../index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-sign-out-alt"></i></div>
+                        Shko ne faqen kryesores
+                    </a>
+                    <!-- <a class="nav-link" href="#">
+              <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+              Dashboard
+            </a> -->
+                    <!-- <div class="sb-sidenav-menu-heading">Interface</div>
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+              <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+              Pages
+              <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+            </a>
+            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                  Authentication
+                </a>
+              </nav>
+            </div> -->
+                </div>
+            </div>
+
+            <!-- <div class="sb-sidenav-footer">
+                <div class="small">Admini: <?php //echo $username; 
+                                            ?></div>
+            </div>
+             -->
+        </nav>
+    </div>
+
+
+    <script>
+        (function($) {
+            "use strict";
+
+            // Shtoni gjendje aktive në sidebar nav links
+            var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+                if (this.href === path) {
+                    $(this).addClass("active");
+                }
+            });
+
+            // NNdërroni anën e navigation
+            $("#sidebarToggle").on("click", function(e) {
+                e.preventDefault();
+                $("body").toggleClass("sb-sidenav-toggled");
+            });
+        })(jQuery);
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
