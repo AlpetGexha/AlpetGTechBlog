148cc32943fd772747ea99e939c26f03cabbf803
Alpet Gexha <agexha@gmail.com>
2021-05-29 00:57:17 +0200

    smoth scroll

--- assets/php/head.php
+++ assets/php/head.php
@@ -17,12 +17,30 @@
 
    <!-- ------------ icone ------------------ -->
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
    <script>
-      addEventListener("load", function() {
-         setTimeout(hideURLbar, 0);
-      }, false);
+      $(document).ready(function() {
+         // Add smooth scrolling to all links
+         $("a").on('click', function(event) {
 
-      function hideURLbar() {
-         window.scrollTo(0, 1);
-      }
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+               // Prevent default anchor click behavior
+               event.preventDefault();
+
+               // Store hash
+               var hash = this.hash;
+
+               // Using jQuery's animate() method to add smooth page scroll
+               // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+               $('html, body').animate({
+                  scrollTop: $(hash).offset().top
+               }, 800, function() {
+
+                  // Add hash (#) to URL when done scrolling (default click behavior)
+                  window.location.hash = hash;
+               });
+            } // End if
+         });
+      });
    </script>
