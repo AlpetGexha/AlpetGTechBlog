3fcb474a6aaecb57f759958212e2283b67bc182d
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-28 17:03:50 +0200

    ADD delete for category

--- admin/create_category.php
+++ admin/create_category.php
@@ -1,6 +1,8 @@
 <?php
+$msg = "";
 include "../server.php";
 include "../database/config.php";
+
 ?>
 
 <?php get_Aheader("Admin"); ?>
@@ -27,7 +29,7 @@ include "../database/config.php";
 
                         <form method="POST" action="#">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="Shto Kategori" name="c_kategory" required="" aria-describedby="button-addon2" oninvalid="this.setCustomValidity('Shkruani kategorin');" oninput="this.setCustomValidity('');">
+                                <input type="text" class="form-control" placeholder="Shto Kategori" name="c_kategory" required="" autofocus="" aria-describedby="button-addon2" oninvalid="this.setCustomValidity('Shkruani kategorin');" oninput="this.setCustomValidity('');">
                                 <button class="btn btn-outline-primary" type="submit" name="add_kategory" id="button-addon2">Shto</button>
                             </div>
                         </form>
@@ -39,6 +41,7 @@ include "../database/config.php";
                         <tr>
                             <th scope="col">ID</th>
                             <th scope="col">Kategorit</th>
+                            <th scope="col">Opsionet</th>
                         </tr>
                     </thead>
 
@@ -49,12 +52,37 @@ include "../database/config.php";
                         if ($result = mysqli_query($db, $c_sql)) {
                             $i = 1;
                             foreach ($result as $key => $c_row) {
-                                
-                                //$c_row['id'] 
+
+                                //$c_row['id']
                                 echo ' <tr>
                         <td> ' . $i++. ' </td> 
                         <td> ' . $c_row['emri'] . ' </td>
+                        <td> <a class="btn btn-danger"  data-toggle="modal" data-target="#aplikemet_' . $c_row["id"] . ' ">Fshije</a> </td>
                         </tr>  ';
+
+                                echo '
+              <div class="modal fade" id="aplikemet_' . $c_row['id'] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLongTitle">Kujedes</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      A jeni i sigurt q&euml; d&euml;shironi ta fshini k&euml;t&euml; kategori<br>
+                      N&euml;se fshini k&euml;t&euml; kategori do te fshini edhe t&euml; gjita  postimet me k&euml;te kategori
+                      <div class="modal-footer">
+                       <button type="button" class="btn btn-secondary" data-dismiss="modal">JO</button>
+                        <a href="delete/catefory_delete.php? id= '.$c_row['id'].'"class="btn btn-danger"id="delete_btn" >PO! Fshije</a> 
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+              ';
                             }
                         }
 
---
+++ admin/delete/catefory_delete.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../../database/config.php";
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "DELETE  FROM post_categories WHERE  id = '$id'";
+    $result = mysqli_query($db, $sql);
+
+    $delete = "DELETE from post where id='$id'";
+    $result = mysqli_query($db,$delete);
+
+    if (!$result == TRUE) {
+        $msg = "False";
+    } else{
+        header('Location: ../create_category.php');
+        $msg = "True";
+    }
+
+}
+
