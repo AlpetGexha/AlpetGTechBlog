fc7042836403ae28ecf4ca8d26b6e13e0225cc67
Alpet Gexha <agexha@gmail.com>
2021-05-28 22:21:16 +0200

    style for error

--- errors/500.php
+++ errors/500.php
@@ -1,7 +1,63 @@
-<div style="text-align: center;">
-    <h1>
-        Error 500
-        Internal Server Error <br>
-    </h1>
-    <a href="../index.php">Kthehu ne faqen kryesore</a>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error 500 - AlpetGTechBlog</title>
+</head>
+<body>
+<div id="main">
+    	<div class="fof">
+        		<h1>Error 500</h1><h2>Internal Server Error</h2><br>
+        		<a href="../index.php">Kthehu ne faqen kryesore</a>
+    	</div>
 </div>
+</body>
+</html>
+<style type="text/css">
+	*{
+    transition: all 0.6s;
+}
+
+html {
+    height: 100%;
+}
+
+body{
+    font-family: 'Lato', sans-serif;
+    color: #888;
+    margin: 0;
+}
+
+#main{
+    display: table;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+.fof{
+	  display: table-cell;
+	  vertical-align: middle;
+}
+
+.fof h1{
+	  font-size: 50px;
+	  display: inline-block;
+	  padding-right: 12px;
+	  animation: type .5s alternate infinite;
+}
+.fof h2{
+	  font-size: 50px;
+	  display: inline-block;
+	  padding-right: 12px;
+}
+
+.fof a{
+	column-rule: #888;
+	text-decoration: none;
+}
+
+@keyframes type{
+	  from{box-shadow: inset -3px 0px 0px #888;}
+	  to{box-shadow: inset -3px 0px 0px transparent;}
+}
+</style>
