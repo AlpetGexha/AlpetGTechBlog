4019151c9575828b4b6ad1d379d24f3ca653e1ce
Alpet Gexha <agexha@gmail.com>
2021-05-29 16:52:01 +0200

    Update head.php

--- assets/php/head.php
+++ assets/php/head.php
@@ -17,7 +17,6 @@
 
    <!-- ------------ icone ------------------ -->
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
    <script>
       $(document).ready(function() {
          // Add smooth scrolling to all links
