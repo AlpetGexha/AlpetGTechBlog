866af72836d5d505e2d451b37cccb5972e34acbb
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-06-06 21:30:07 +0200

    sticky widget

--- assets/php/widget.php
+++ assets/php/widget.php
@@ -1,40 +1,40 @@
-<div class="col-lg-4 text-left mt-3">
-    <div class="text-left">
+<div class="col-lg-4 text-left mt-3 ">
+    <div class="widget-sticky">
         <h4><strong>Kategorit</strong></h4>
         <ul class="list-group mt-2">
             <?php get_kadegoirt("post_categories", "post"); ?>
         </ul>
-    </div>
-    <div class="widget_social mt-3">
+        <div class="widget_social mt-3">
 
-        <h4>Rrjetet Sociale</h4>
-        <ul>
-            <li>
-                <a class="facebook" href="">
-                    <i class="fab fa-facebook-f"></i>
-                    <span class="count"></span> Facebook</a>
-            </li>
+            <h4>Rrjetet Sociale</h4>
+            <ul>
+                <li>
+                    <a class="facebook" href="">
+                        <i class="fab fa-facebook-f"></i>
+                        <span class="count"></span> Facebook</a>
+                </li>
 
-            <li>
-                <a class="instagram" href="">
-                    <i class="fab fa-instagram"></i>
+                <li>
+                    <a class="instagram" href="">
+                        <i class="fab fa-instagram"></i>
 
-                    <span class="count"></span> instagram</a>
-            </li>
+                        <span class="count"></span> instagram</a>
+                </li>
 
-            <li>
-                <a class="twitter" href="">
-                    <i class="fab fa-twitter"></i>
-                    <span class="count"></span> Twitter</a>
-            </li>
+                <li>
+                    <a class="twitter" href="">
+                        <i class="fab fa-twitter"></i>
+                        <span class="count"></span> Twitter</a>
+                </li>
 
 
-            <li>
-                <a class="github" href="">
-                    <i class="fab fa-github"></i>
-                    <span class="count"></span> github</a>
-            </li>
+                <li>
+                    <a class="github" href="">
+                        <i class="fab fa-github"></i>
+                        <span class="count"></span> github</a>
+                </li>
 
-        </ul>
+            </ul>
+        </div>
     </div>
 </div>
