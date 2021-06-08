041ed3c91efcfa2ef65320395ce129f6f25cf302
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-06-09 01:31:09 +0200

    Update head.php

--- assets/php/head.php
+++ assets/php/head.php
@@ -17,29 +17,3 @@
 
    <!-- ------------ icone ------------------ -->
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-   <script>
-      $(document).ready(function() {
-         // Add smooth scrolling to all links
-         $("a").on('click', function(event) {
-
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-               // Prevent default anchor click behavior
-               event.preventDefault();
-
-               // Store hash
-               var hash = this.hash;
-
-               // Using jQuery's animate() method to add smooth page scroll
-               // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-               $('html, body').animate({
-                  scrollTop: $(hash).offset().top
-               }, 800, function() {
-
-                  // Add hash (#) to URL when done scrolling (default click behavior)
-                  window.location.hash = hash;
-               });
-            } // End if
-         });
-      });
-   </script>
